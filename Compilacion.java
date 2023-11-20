e1e41f47d2e1cc944aeb5bc6f8d70806301db565
jgomezf <jgomezf@unal.edu.co>
2023-11-20 10:27:20 -0500

    FirstUpdate

---
+++ Compilacion.java
@@ -0,0 +1,5 @@
+public Compilacion{
+    public static void public static void main(String[]args){
+        Compilador comp = new Compilador("Eje1");
+    }
+}
---
+++ Compilador.java
@@ -0,0 +1,9 @@
+public Compilador{
+    private String NameProgram;
+
+    public Compilador(String NameProgram){
+        NameProgram.this = NameProgram;
+    }
+
+
+}
